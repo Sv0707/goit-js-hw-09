4b0a2762dcae31d2ad488a0911641174a0fccbb2
Svitlana <87758266+Sv0707@users.noreply.github.com>
2021-10-04 21:30:56 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,29 +1,31 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    setTimeout(() => {
-      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      console.log(position, delay);
-    }, delay);
-  } else {
-    setTimeout(() => {
-      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      console.log(position, delay);
-    }, delay);
+const createPromise = (position, delay) => {
+  new Promise((resolve, reject) => {
+    
+    setTimeout(() => {      
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(console.log(`✅ Fulfilled promise ${position} in ${delay}ms`));
+      } 
+      else {
+        reject(console.log(`❌ Rejected promise ${position} in ${delay}ms`));
+          }
+        }, delay);
+
   }
+)
 }
 
 const refs = {
-  form: document.querySelector('.form'),
-};
+  form: document.querySelector(".form"),
+}
 
-refs.form.addEventListener('submit', e => {
+refs.form.addEventListener("submit", e => {
   e.preventDefault();
-  const amount = parseInt(e.currentTarget.elements.amount.value);
-  const step = parseInt(e.currentTarget.elements.step.value);
-  let delay = parseInt(e.currentTarget.elements.delay.value);
-  for (let i = 0; i < amount; i++) {
-    createPromise(i, delay);
-    delay += step;
-  }
-});
+const amount = parseInt(e.currentTarget.elements.amount.value);
+const step = parseInt(e.currentTarget.elements.step.value);
+let delay = parseInt(e.currentTarget.elements.delay.value);
+for (let i = 0; i < amount; i++) {
+  createPromise(i, delay);
+  delay += step;
+}
+})
